10196b1c9b0241d973cc5f804b27af1c9e8a3878
sagadio1 <46498456+sagadio1@users.noreply.github.com>
2019-01-09 04:41:52 +0100

    Fix erro 11
    se cambia la condicion de la opcion 1 y 2

--- logic/functions/functions.c
+++ logic/functions/functions.c
@@ -52,7 +52,7 @@ void planTrip(List *l){
         printPlanMenu();
         option = askUserForOption();
 
-		if (destination < 1 || destination > 2) {
+		if (option < 1 || option > 2) {
 			printOptionError();
 		}
     } while (option < 1 || option > 2);
